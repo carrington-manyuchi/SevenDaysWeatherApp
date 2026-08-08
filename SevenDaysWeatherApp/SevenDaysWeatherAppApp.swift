251dacd4319003b34659aab00f0475177b9c5624
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-08-08 09:28:52 +0200

    Initial Commit

---
+++ SevenDaysWeatherApp/SevenDaysWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenDaysWeatherAppApp.swift
+//  SevenDaysWeatherApp
+//
+//  Created by Manyuchi, Carrington C on 2026/08/08.
+//
+
+import SwiftUI
+
+@main
+struct SevenDaysWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
