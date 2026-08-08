63942ec1c839967f7f26154d922683664980251a
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-08-08 09:59:51 +0200

    Learning Objectives:

--- SevenDaysWeatherApp/SevenDaysWeatherAppApp.swift
+++ SevenDaysWeatherApp/SevenDaysWeatherAppApp.swift
@@ -7,6 +7,27 @@
 
 import SwiftUI
 
+
+/**
+ 
+ Learning Objectives:
+   - SwiftUI
+   - View Composition
+   - Using UIKit Components in SwiftUI
+   - MVVM
+   - Using Combine to publish and Observe Async responses
+   - Passing Obseervable and Observed Objects
+
+   - Generic Network API calls
+   - Use of Result Type
+ 
+   - Lottie ANimations
+   - Fetching Coordinates from Adress
+   - Swift Package manager
+   - Date and time Formatters
+ 
+ */
+
 @main
 struct SevenDaysWeatherAppApp: App {
     var body: some Scene {
